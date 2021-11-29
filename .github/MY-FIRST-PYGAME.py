cf7a609b37359b9d3fdee58e56e319a1936b4e93
Amari Mayfield <70582293+s11628195@users.noreply.github.com>
2021-11-29 14:37:23 -0500

    v0.2
    Started pygame.inti(), setup color value, created window caption.

--- .github/MY-FIRST-PYGAME.py
+++ .github/MY-FIRST-PYGAME.py
@@ -1,1 +1,25 @@
-# My First PyGame, Amari Mayfield, 11/29/21 1:59, v0.0
+# My First PyGame, Amari Mayfield, 11/29/21 1:59, v0.1
+
+import pygame, sys
+from pygame.locals import *
+
+# Start PyGame
+pygame.init()
+
+# Setup our Window. l
+windowSurface = pygame.display.set_mode((500,400),0,32)
+pygame.display.set_caption('Hello, world')
+
+# Setup Colors
+BLACK = (0,0,0)
+WHITE = (255, 255, 255)
+RED = (255,0,0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+# Setup font.
+basicFont = pygame.font.SysFont(None, 48)
+
+# Setup Text.
+text = basicFont.render('Hello, world!', True, WHITE, BLUE)
+textRect = text.get_rect()
