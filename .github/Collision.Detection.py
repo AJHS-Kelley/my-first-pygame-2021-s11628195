4ad15d228adbdb5de1bb2a44bcd31a4604a08ad0
Amari Mayfield <70582293+s11628195@users.noreply.github.com>
2022-01-21 13:52:42 -0500

    v0.1

--- .github/Collision.Detection.py
+++ .github/Collision.Detection.py
@@ -1,1 +1,8 @@
-Pygame Collision Detection Pactice, Aamri Mayfield, Jan 19, 2022, 2:06pm, v0.0
+Pygame Collision Detection Pactice, Aamri Mayfield, Jan 19, 2022, 2:06pm, v0.1
+
+import pygame, sys, random
+from pygame.locals import *
+
+# Setup Pygame
+pygame.init()
+mainClock = pygame.time.Clock ()
