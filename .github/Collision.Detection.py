0df351ababd1c3d7ec7d09a005636e1985e414e6
Amari Mayfield <70582293+s11628195@users.noreply.github.com>
2022-02-01 18:38:59 -0500

    v0.9

--- .github/Collision.Detection.py
+++ .github/Collision.Detection.py
@@ -71,6 +71,16 @@ While True:
                 
                 if event.type == MOUSEBUTTONUP:
                     foods.append(pygame.Rect)(event.pos{1}), FOODSIZE, FOODSIZE))
+    foodCounter += 1
+    if foodCounter => NEWFOOD:
+       # Add new food
+       foodCounter = 0
+       foods.append(pygame.Rect(random.randit)(0, WINDOWWIDTH - FOODSIZE), random.randit(0, WINDOWHEIGHT - FOODSIZE), FOODSIZE, FOODSIZE))
+
+    # Draw white background on Window Surface.
+    windowSurface.fill(WHITE)
+    
+                    
 
                     
 
